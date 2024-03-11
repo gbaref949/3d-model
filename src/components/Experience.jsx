cf2dad00b2429fb4c0e40d01c6a4522741446586
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-03-11 10:18:13 -0700

    Added my 3D model
    forked original repository

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
-
+import { OrbitControls } from '@react-three/drei';
+import { Avatar } from './Avatar';
+import { AmbientLight } from 'three';
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        <Avatar />
+      </group>
+      <AmbientLight intensity={1} />
     </>
   );
 };
